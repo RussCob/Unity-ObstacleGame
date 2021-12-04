c99621e07ee848e169d27e9ba57dc16966ca84b9
Blackfb <resul.cobanoglu@outlook.com>
2021-12-03 23:08:00 -0500

    U6

--- OBSTACLE 3D/New Unity Project/Assets/Scenes/Mover.cs
+++ OBSTACLE 3D/New Unity Project/Assets/Scenes/Mover.cs
@@ -13,7 +13,7 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
