2fd281001dd634339376a0249171d707ae6aad1e
Blackfb <resul.cobanoglu@outlook.com>
2021-12-08 22:01:02 -0500

    U10

--- OBSTACLE 3D/New Unity Project/Assets/Scenes/ObjectHit.cs
+++ OBSTACLE 3D/New Unity Project/Assets/Scenes/ObjectHit.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    // 
+    // Add Collison note on the console
     private void OnCollisionEnter(Collision other) 
     {
-        Debug.Log("Bumped into a wall");
+        Debug.Log("Bumped into a wall");  
+        //Change color
+        GetComponent<MeshRenderer>().material.color = Color.red;
         
     }
 }
