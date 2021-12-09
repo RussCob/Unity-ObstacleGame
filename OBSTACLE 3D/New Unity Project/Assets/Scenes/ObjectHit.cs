f85dc8f67a568a590485ab96a207c3316f05716e
Blackfb <resul.cobanoglu@outlook.com>
2021-12-08 22:25:43 -0500

    U11
    What happened so far;
    Created playground ,
    Created new scripts for objects and player,
    Added player,
    Added walls,
    Added Color,
    Color changes When player hit the walls,
    Added new object,
    Added console log for warning,
    Added score and it shows how many times player hits to the objects and walls,

--- OBSTACLE 3D/New Unity Project/Assets/Scenes/ObjectHit.cs
+++ OBSTACLE 3D/New Unity Project/Assets/Scenes/ObjectHit.cs
@@ -7,7 +7,7 @@ public class ObjectHit : MonoBehaviour
     // Add Collison note on the console
     private void OnCollisionEnter(Collision other) 
     {
-        Debug.Log("Bumped into a wall");  
+       
         //Change color
         GetComponent<MeshRenderer>().material.color = Color.red;
         
